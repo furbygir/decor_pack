2fc107ff0d6d03e0e391785adb5b10840337f5d2
Ava <154105735+furbygir@users.noreply.github.com>
2025-03-30 19:16:58 +1000

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,288 @@
+minetest.register_node('decor_pack:battery_stack', {
+	description = '30 Stacked amazan rechargable batteries',
+	drawtype = 'mesh',
+	mesh = 'baes.obj',
+	tiles = {'amazan.png'},
+	groups = {oddly_breakable_by_hand=10, dig_stone=5},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
+minetest.register_node('decor_pack:bath', {
+	description = 'Bathtub with Bubbles',
+	drawtype = 'mesh',
+	mesh = 'bath.obj',
+	tiles = {'bath.png'},
+	groups = {oddly_breakable_by_hand=10, dig_stone=5},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
+minetest.register_node('decor_pack:battery_pack', {
+	description = '30 Pack amazan rechargable batteries',
+	drawtype = 'mesh',
+	mesh = 'battery.obj',
+	tiles = {'amazan.png'},
+	groups = {oddly_breakable_by_hand=10, dig_stone=5},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
+minetest.register_node('decor_pack:bee', {
+	description = 'Bee',
+	drawtype = 'mesh',
+	mesh = 'bee.obj',
+	tiles = {'bee.png'},
+	groups = {oddly_breakable_by_hand=1},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
+minetest.register_node('decor_pack:beehive', {
+	description = 'Beehive',
+	drawtype = 'mesh',
+	mesh = 'beehive.obj',
+	tiles = {'beehive.png'},
+	groups = {oddly_breakable_by_hand=10, dig_stone=5},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
+minetest.register_node('decor_pack:boombox', {
+	description = 'Boombox',
+	drawtype = 'mesh',
+	mesh = 'boombox.obj',
+	tiles = {'boombox.png'},
+	groups = {oddly_breakable_by_hand=10, dig_stone=5},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
+minetest.register_node('decor_pack:dj_table', {
+	description = 'DJ Table',
+	drawtype = 'mesh',
+	mesh = 'dj.obj',
+	tiles = {'dj.png'},
+	groups = {oddly_breakable_by_hand=10, dig_stone=5},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
+minetest.register_node('decor_pack:furby_pink', {
+	description = 'Pink Furby',
+	drawtype = 'mesh',
+	mesh = 'furby.obj',
+	tiles = {'furby.png'},
+	groups = {oddly_breakable_by_hand=3},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
+minetest.register_node('decor_pack:hammer', {
+	description = 'Hammer',
+	drawtype = 'mesh',
+	mesh = 'hammer.obj',
+	tiles = {'ham.png'},
+	groups = {oddly_breakable_by_hand=10, dig_stone=5},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
+minetest.register_node('decor_pack:lego_mario', {
+	description = 'Lego Mario',
+	drawtype = 'mesh',
+	mesh = 'legomario.obj',
+	tiles = {'lego_mario.png'},
+	groups = {oddly_breakable_by_hand=10, dig_stone=5},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
+minetest.register_node('decor_pack:kamtape', {
+	description = 'Kamtape Link',
+	drawtype = 'mesh',
+	mesh = 'kamtape.obj',
+	tiles = {'green.png'},
+	groups = {oddly_breakable_by_hand=10, dig_stone=5},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
+minetest.register_node('decor_pack:miku', {
+	description = 'Miku Blocky',
+	drawtype = 'mesh',
+	mesh = 'miku.obj',
+	tiles = {'Miku.png'},
+	groups = {oddly_breakable_by_hand=10, dig_stone=5},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
+minetest.register_node('decor_pack:miku2', {
+	description = 'Miku Fumo',
+	drawtype = 'mesh',
+	mesh = 'miku2.obj',
+	tiles = {'ma.png'},
+	groups = {oddly_breakable_by_hand=10, dig_stone=5},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
+minetest.register_node('decor_pack:miku3', {
+	description = 'Miku toy',
+	drawtype = 'mesh',
+	mesh = 'miku4.obj',
+	tiles = {'mikubody.png', 'mikuleft.png'},
+	groups = {oddly_breakable_by_hand=10, dig_stone=5},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
+minetest.register_node('decor_pack:sonic', {
+	description = 'Sonic, The Hedgehog',
+	drawtype = 'mesh',
+	mesh = 'sonic.obj',
+	tiles = {'sonic.png'},
+	groups = {oddly_breakable_by_hand=10, dig_stone=5},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
+minetest.register_node('decor_pack:toothbrush', {
+	description = 'Blue Toothbrush',
+	drawtype = 'mesh',
+	mesh = 'toothbrush.obj',
+	tiles = {'colo.png'},
+	groups = {oddly_breakable_by_hand=10},
+	paramtype = 'light',
+	paramtype2 = 'facedir',
+	selection_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		},
+	collision_box = {
+		type = 'fixed',
+		fixed = {-.5, -.5, -.5, .5, .5, .5},
+		}
+})
+
